f64de73bb3cb8ae7974323c09e51ea76554f38e1
kazshiomi <pontaxdesu@gmail.com>
2024-08-09 10:26:31 +0900

    休日区分を選択したときに休日日数が自動入力され、始業・就業時間をクリアする処理を追加

--- timecard/js/tcd020.js
+++ timecard/js/tcd020.js
@@ -28,6 +28,51 @@ function holComboChange(){
     if (holValueDisp == 0) {
     	$('#js_holValue').addClass('display_none');
     }
+
+    //--- 追加 2024/08/08 システム開発Gr 塩見
+
+    //休日区分を取得
+    var selectElement = document.getElementsByName("tcd020HolKbn")[0];
+    var selectedOption = selectElement.options[selectElement.selectedIndex];
+    var selectedText = selectedOption.text;
+
+    //休日日数を格納する変数を初期化
+    var holdays = "0.0";
+
+    //始業・終業時間をクリアする処理の制御用フラグ
+    var timeclearFlg = false;
+    
+    //休日日数を格納
+    switch (selectedText){
+    
+        case "午前半休":
+            holdays = "0.5";
+            break;
+        case "午後半休":
+            holdays = "0.5";
+            break;
+        case "振休":
+            holdays = "";
+            break;
+        case "指定なし":
+            holdays = "";
+            break;    
+        default:
+            holdays = "1.0";
+            timeclearFlg = true;
+    }
+    
+    //休日日数を更新
+    document.getElementsByName("tcd020HolDays").item(0).value = holdays;
+
+    //始業・終業時間をクリア
+    if (timeclearFlg){
+        clearValue('tcd020InHour', 'tcd020InMinute'); //始業時間
+        clearValue('tcd020OutHour', 'tcd020OutMinute'); //終業時間
+    }
+
+    //---
+
     setStyle('tcd020HolDays', holDaysDisp);
 }
 
